f0f1ec640a4fd38fbc7007dc1ccd36408b83093a
SinaAfzali <128758290+SinaAfzali@users.noreply.github.com>
2024-05-18 15:24:32 +0330

    delete extra pakages

--- api/controller/user-controller.js
+++ api/controller/user-controller.js
@@ -1,5 +1,8 @@
 const { getDocument } = require('../utilities/db_mongo');
 
+const my_email = 'testemail.comm';
+const my_password = '455411';
+
 const userValidator = async function(req, res) {
 
     const result = await getDocument('users', {userName : String(req.body.userName)});
@@ -11,7 +14,27 @@ const userValidator = async function(req, res) {
 }
 
 const emailsender = async function(req, res){
-  
+    let transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'your-email@gmail.com',
+            pass: 'your-password'
+        }
+    });
+
+    let mailOptions = {
+        from: 'your-email@gmail.com',
+        to: 'recipient-email@example.com',
+        subject: 'Test Email',
+        text: 'This is a test email sent using Nodemailer.'
+    };
+
+    try {
+        let info = await transporter.sendMail(mailOptions);
+        console.log('Email sent: ' + info.response);
+    } catch (error) {
+        console.error('Error sending email: ' + error);
+    }
 }
 
 module.exports = {userValidator, emailsender};
--- api/routes/user-route.js
+++ api/routes/user-route.js
@@ -4,6 +4,7 @@ const {userValidator, emailsender} = require('../controller/user-controller')
 
 /* GET users listing. */
 router.post('/username/validator',userValidator);
-router.post('/email/send', emailsender);
+router.post('/email/sendCode', emailsender);
+router.post('/email/validator', emailsender);
 
 module.exports = router;
