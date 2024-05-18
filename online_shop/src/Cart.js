4ca8f436c764a91e9de93eecc31aa68d94523441
Sasan <SasanNikjoo2083@gmail.com>
2024-05-18 14:48:07 +0430

    commit test

--- online_shop/src/Cart.js
+++ online_shop/src/Cart.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Cart = () => {
   return (
     <div>
-      <h1>Your Shopping Cart</h1>
+      <h1>Your Shopping Cart is ready</h1>
       {/* Add your cart details here */}
     </div>
   );
